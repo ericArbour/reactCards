d3b28d4a515617d836296b2329e60ae3e573a672
Eric Arbour <arboureric@gmail.com>
2017-10-05 15:26:41 -0400

    eslint: section 3 lecture 18

--- src/server/server.js
+++ src/server/server.js
@@ -4,3 +4,20 @@ console.log(`hello ${blegh}`);
 const obj = {hey: 1};
 const obj2 = {...obj, blegh: 2};
 console.log(obj2);
+
+class AppComponent {
+    static PropTypes = {
+        blegh: "whoa"
+    };
+}
+
+switch (blegh) {
+    case 1: 
+        console.log("hey");
+        break;
+    case 2:
+        console.log("you");
+        break;
+}
+
+console.log(<AppComponent />);
