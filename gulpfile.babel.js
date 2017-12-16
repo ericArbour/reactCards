ff082c094d11bef493d975e6ad3b3d5017e91ad8
Eric Arbour <arboureric@gmail.com>
2017-12-15 21:51:40 -0500

    gulp tasks and webpack config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,22 +2,26 @@ import gulp from "gulp";
 import path from "path";
 import rimraf from "rimraf";
 import child_process from "child_process";
+import webpackConfig from "./webpack.config";
+import webpack from "webpack";
+import WebpackDevServer from "webpack-dev-server";
+
 
 const $ = require("gulp-load-plugins")();
 
+
 gulp.task("server:clean", cb => {
-    rimraf("./build", ()=> cb());
+    rimraf("./build", () => cb());
 });
 
-gulp.task("server:build", 
+gulp.task("server:build",
     gulp.series(
         "server:clean",
         compileServer
     )
 );
 
-gulp.task(
-    "server:watch",
+gulp.task("server:watch",
     gulp.series(
         "server:build",
         watchServer
@@ -54,16 +58,16 @@ gulp.task(
     )
 );
 
-function compileServer () {
+function compileServer() {
     return gulp.src("./src/server/**/*.js")
         .pipe($.changed("./build"))
         .pipe($.sourcemaps.init())
         .pipe($.babel())
-        .pipe($.sourcemaps.write(".", {sourceRoot: path.join(__dirname, "src", "server")}))
+        .pipe($.sourcemaps.write(".", { sourceRoot: path.join(__dirname, "src", "server") }))
         .pipe(gulp.dest("./build"));
 }
 
-function watchServer () {
+function watchServer() {
     return gulp
         .watch("./src/server/**/*.js", gulp.series(compileServer))
         .on("error", () => {});
@@ -73,7 +77,8 @@ function runServer() {
     return $.nodemon({
         script: "./server.js",
         watch: "build",
-        ignore: ["**/__tests"]
+        ignore: ["**/__tests"],
+        exec: "node --debug"
     });
 }
 
@@ -81,6 +86,7 @@ function testServer(cb) {
     child_process.exec("node ./tests.js", (err, stdout, stderr) => {
         console.log(stdout);
         console.error(stderr);
+
         if (err) {
             cb(new $.util.PluginError("testServer", "Tests failed"));
         } else {
@@ -95,3 +101,61 @@ function runServerTests() {
         watch: "build"
     });
 }
+
+// ---------
+// Client
+const consoleStats = {
+    colors: true,
+    exclude: ["node_modules"],
+    chunks: false,
+    assets: false,
+    timings: true,
+    hash: false,
+    version: false
+};
+
+gulp.task("client:clean", cb => {
+    rimraf("./public/build", () => cb());
+});
+
+gulp.task(
+    "client:build",
+    gulp.series(
+        "client:clean",
+        buildClient
+    ));
+
+gulp.task(
+    "client:dev",
+    gulp.series(
+        "client:clean",
+        watchClient
+    ));
+
+function buildClient(cb) {
+    webpack(webpackConfig, (err, stats) => {
+        if (err) {
+            cb(err);
+            return;
+        }
+        console.log(stats.toString(consoleStats));
+        cb();
+    });
+}
+
+function watchClient() {
+    const compiler = webpack(webpackConfig);
+    const server = new WebpackDevServer(compiler, {
+        publicPath: "/build/",
+        hot: true,
+        stats: consoleStats,
+        headers: { "Access-Control-Allow-Origin": "*" }
+    });
+
+    server.listen(8080, () => {});
+}
+
+// ------
+// Other Tasks
+gulp.task("dev", gulp.parallel("server:dev", "client:dev"));
+gulp.task("build", gulp.parallel("server:build", "client:build"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
-var path = require('path'),
-     _ = require('lodash'),
-     webpack = require('webpack'),
-     ExtractTextPlugin = require('extract-text-webpack-plugin');
+let path = require("path"),
+    _ = require("lodash"),
+    webpack = require("webpack"),
+    ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const vendor = [
-    'lodash'
+    "lodash",
+    "react",
+    "react-dom",
+    "react-router",
+    "socket.io-client",
+    "rxjs"
 ];
 
 function createConfig(isDebug) {
-    const devtool = isDebug ? "cheap-module-source-map" : null;
+    const devtool = isDebug ? "eval-source-map" : false;
     const plugins = [
-        new webpack.optimize.CommonsChunkPlugin({name:"vendor", filename:"vendor.js",}),
+        new webpack.optimize.CommonsChunkPlugin({name: "vendor", filename: "vendor.js"}),
         new webpack.DefinePlugin({
             "process.env": {
                 NODE_ENV: `"${process.env.NODE_ENV || "development"}"`
@@ -19,21 +24,38 @@ function createConfig(isDebug) {
             IS_DEVELOPMENT: isDebug
         })
     ];
+
     const loaders = {
-        js: { test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/ },
-        eslint: { test: /\.jsx?$/, loader: 'eslint', exclude: /node_modules/ },
-        json: { test: /\.json?$/, loader: 'json' },
-        css: { test: /\.css?$/, loader: 'style!css?sourceMap' },
-        sass: { test: /\.scss?$/, loader: 'style!css?sourceMap!sass?sourceMap' },
-        files: { test: /\.(png|jpg|jpeg|gif|woff|ttf|eot|svg|woff2)/, loader: 'urlLoader?limit=5000' }
+        js: 	{test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/},
+        eslint: {test: /\.jsx?$/, loader: "eslint-loader", exclude: /node_modules/},
+        json: 	{test: /\.json$/, loader: "json-loader"},
+        css: 	{test: /\.css$/, loader: "style-loader!css-loader?sourceMap"},
+        sass: 	{test: /\.scss$/, loader: "style-loader!css-loader?sourceMap!sass-loader?sourceMap"},
+        files:	{test: /\.(png|jpg|jpeg|gif|woff|ttf|eot|svg|woff2)/, loader: "url-loader?limit=500"}
     };
-    const clientEntry = ["./src/client/client.js"];
+
+    const clientEntry = ["babel-polyfill", "./src/client/client.js"];
     let publicPath = "/build/";
     if (isDebug) {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new webpack.NamedModulesPlugin());
+        clientEntry.unshift(
+            "react-hot-loader/patch",
+            "webpack-dev-server/client?http://localhost:8080/",
+            "webpack/hot/only-dev-server"
+        );
+        publicPath = "http://localhost:8080/build/";
 
     } else {
+        plugins.push(
+            new ExtractTextPlugin("[name].css"),
+            new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
+        );
+        loaders.css.loader = ExtractTextPlugin.extract({ fallback: "style-loader", use: "css-loader"});
+        loaders.sass.loader = ExtractTextPlugin.extract({ fallback: "style-loader", use: "css-loader!sass-loader"});
 
-    }
+    }   
+
     return {
         name: "client",
         devtool,
@@ -47,17 +69,17 @@ function createConfig(isDebug) {
             publicPath
         },
         resolve: {
-            extensions: ["", ".js", ".jsx"],
+            extensions: ["*", ".js", ".jsx"],
             alias: {
                 shared: path.join(__dirname, "src", "server", "shared")
             }
         },
         module: {
-            loaders: _.values(loaders)
+            rules: _.values(loaders)
         },
         plugins
     };
 }
 
 module.exports = createConfig(process.env.NODE_ENV !== "production");
-module.exports.create = createConfig;
+// module.exports.create = createConfig();
