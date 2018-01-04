62fabefe5b01db3875c16f56d37d959a4494f8d0
Eric Arbour <arboureric@gmail.com>
2018-01-03 20:41:03 -0500

    video 45 container base

--- src/client/client.js
+++ src/client/client.js
@@ -4,15 +4,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {Router, browserHistory as history} from "react-router";
 
+import {Dispatcher} from "shared/dispatcher";
+import * as A from "./actions";
+import {StateProvider} from "./lib/component";
+import createStores from "./stores";
+
+// ---------------------------
+// Services
+const dispatcher = new Dispatcher();
+const services = {dispatcher};
+
+// ---------------------------
+// Stores
+const stores = createStores(services);
 
 // ---------------------------
 // Render
 function main() {
     const routes = require("./routes").default();
     ReactDOM.render(
-        <Router history={history}>
-            {routes}
-        </Router>,
+        <StateProvider stores={stores} services={services}>
+            <Router history={history}>
+                {routes}
+            </Router>
+        </StateProvider>,
         document.getElementById("mount"));
 }
 
